130c0cf234d71ad9f38d0eaf69fb9977c32d5740
Jamie Ingram <jr-ingram@hotmail.co.uk>
2023-08-30 11:11:45 +0100

    Add threshold and pixel grid which is fed into bowyer watson

--- imageProcessing.ts
+++ imageProcessing.ts
@@ -1,24 +1,30 @@
 import { Image } from 'image-js'
+import { Vertex } from './types'
 
 export const loadImage = async (path: string): Promise<Image> => {
     const image = await Image.load(path)
-    console.log(getPixelGrid(image))
     return image
 }
 
-const getPixelGrid = (image: Image): number[][][] => {
-    const { height, width } = image;
-    const pixelArray = image.getPixelsArray()
+export const getPixelGrid = (image: Image): number[][][] => {
+    const { width, height } = image
     const pixelGrid = []
-    for (let i = 0; i < height; i++) {
-        const pixelRow = []
-        for (let j = 0; j < width; j++) {
-            pixelRow.push(pixelArray[i * j])
+    for (let y = 0; y < height; y++) {
+        const pixelRow: number[][] = []
+        for (let x = 0; x < width; x++) {
+            const pixel = image.getPixelXY(x, y)
+            pixelRow.push(pixel)
         }
         pixelGrid.push(pixelRow)
     }
 
     return pixelGrid
+}
+
+export const blurImage = async (image: Image): Promise<Image> => {
+    const blurredImage = image.blurFilter()
+    await blurredImage.save('blurred.png')
+    return blurredImage
 }
 
 export const greyscaleImage = async (image: Image): Promise<Image> => {
@@ -32,13 +38,7 @@ export const edgeDetect = async (image: Image): Promise<Image> => {
     try {
         const edgeDetectedImage = image.sobelFilter()
         await edgeDetectedImage.save('edgeDetectedImage.png')
-        const threshold = edgeDetectedImage.getThreshold()
-        const thesholdedImage = edgeDetectedImage.mask({
-            algorithm: 'threshold',
-            threshold
-        })
-        await thesholdedImage.save('thresholdedImage.png')
-        return thesholdedImage
+        return edgeDetectedImage
     } catch (err) {
         console.log('error during edge detection')
         console.log(err)
@@ -46,11 +46,48 @@ export const edgeDetect = async (image: Image): Promise<Image> => {
     }
 }
 
-export const getEdgeDetectedPixelGrid = async (path: string): Promise<number[][][]> => {
+export const manualThreshold = async (image: Image): Promise<Image> => {
+    // TODO add smart thresholding thresholding
+    //      threshholding caused issues with getting image pixel data
+    // const threshold = edgeDetectedImage.getThreshold()
+    // const thesholdedImage = edgeDetectedImage.mask({
+    //     algorithm: 'threshold',
+    //     threshold
+    // })
+    const threshold = 128 // arbitrary number
+    const { width, height } = image
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            const pixel = image.getPixelXY(x, y)
+            const thresholdedPixel = pixel[0] < threshold ? 0 : 255
+            image.setPixelXY(x, y, [thresholdedPixel])
+        }
+    }
+    await image.save('thresholdedImage.png')
+    return image
+}
+
+export const getEdgeDetectedPixelGrid = async (path: string): Promise<Image> => {
     const image = await loadImage(path)
-    const greyedImage = await greyscaleImage(image)
-    console.log(getPixelGrid(greyedImage))
-    const edgeDetectedImage = await edgeDetect(greyedImage)
-    const pixelGrid = getPixelGrid(edgeDetectedImage)
-    return pixelGrid
+    const blurredImage = await blurImage(image)
+    const greyedImage = await greyscaleImage(blurredImage)
+    const thesholdedImage = await edgeDetect(greyedImage)
+    return thesholdedImage
+}
+
+export const convertPixelGridToVerticies = (pixelGrid: number[][][]) => {
+    const verticies: Vertex[] = []
+    pixelGrid.forEach((pixelRow, y) => {
+        pixelRow.forEach((pixel, x) => {
+            if (pixel[0] > 0) {
+                const vertex: Vertex = {
+                    x,
+                    y
+                }
+                verticies.push(vertex)
+            }
+        })
+    })
+
+    return verticies;
 }
--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { bowyerWatson } from './triangulation'
-import { getEdgeDetectedPixelGrid } from './imageProcessing'
+import { getEdgeDetectedPixelGrid, manualThreshold, getPixelGrid, convertPixelGridToVerticies } from './imageProcessing'
 
 const pointsToTest = [
     { x: 0, y: 1 },
@@ -18,7 +18,17 @@ triangulation.forEach((triangle) => {
 })
 
 const execute = async (): Promise<void> => {
-    await getEdgeDetectedPixelGrid('testImage.png')
+    console.log('start')
+    const edgeDetectedImage = await getEdgeDetectedPixelGrid('testImage.png')
+    const thresholdedImage = await manualThreshold(edgeDetectedImage)
+    const pixelGrid = getPixelGrid(thresholdedImage)
+    const imageEdgeVerticies = convertPixelGridToVerticies(pixelGrid)
+    // console.log(imageEdgeVerticies)
+    const edgeDetectedTriangulation = bowyerWatson(imageEdgeVerticies)
+    // console.log(edgeDetectedTriangulation.length)
+    console.log('fin')
 }
 
-execute().catch(() => 0)
+execute().catch((err) => {
+    console.log(err)
+})
