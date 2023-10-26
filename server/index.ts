96077ab6a83be2d056e4877c1a166165054f0670
Jamie Ingram <jr-ingram@hotmail.co.uk>
2023-10-26 20:26:40 +0100

    Move existing files to server directory

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,43 @@
+import express from 'express'
+import cors from 'cors'
+import multer from 'multer';
 import { triangulateImage } from './imageTriangulation'
 
-triangulateImage().catch((err) => {
-    console.log(err)
+const app = express()
+const port = 3001
+
+app.use(cors())
+app.use(express.json())
+
+const storage = multer.diskStorage({
+    destination: 'files/',
+    filename: (
+        req,
+        file,
+        callback
+    ) => {
+        callback(null, file.originalname)
+    }
+})
+
+const upload = multer({
+    storage
+})
+
+app.get('/', (req, res) => {
+    console.log('request got!')
+    console.log(`file name: ${req.body}`)
+    triangulateImage().catch((err) => {
+        console.log(err)
+    })
+})
+
+app.post('/', upload.single('image'), (req, res) => {
+    console.log('request got')
+    console.log(`file name: ${JSON.stringify(req.file)}`)
+    res.json({ success: true }).status(200)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
