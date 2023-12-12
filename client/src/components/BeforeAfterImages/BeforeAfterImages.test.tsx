da976e2e93e85cbad05d8bc17df4b4a6cb587615
Jamie Ingram <jr-ingram@hotmail.co.uk>
2023-12-12 18:46:48 +0000

    Add tests for BeforeAfterImages

---
+++ client/src/components/BeforeAfterImages/BeforeAfterImages.test.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { BeforeAfterImages } from "./BeforeAfterImages";
+
+describe("BeforeAfterImages", () => {
+  it("Renders Images and Labels", () => {
+    render(<BeforeAfterImages imageId={0} />);
+    const beforeLabel = screen.getByText("Original Image:");
+    const beforeImage = screen.getByTestId("before-image");
+    const afterLabel = screen.getByText("Triangulated Image:");
+    const afterImage = screen.getByTestId("after-image");
+    expect(beforeLabel).toBeVisible();
+    expect(beforeImage).toBeVisible();
+    expect(afterLabel).toBeVisible();
+    expect(afterImage).toBeVisible();
+  });
+});
